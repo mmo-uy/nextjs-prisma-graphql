28659398738e1b57b859ba5267f24c896c29e153
Maxi Morales <39443928+mmo-uy@users.noreply.github.com>
2023-11-28 22:04:48 -0300

    first commit

---
+++ data/links.ts
@@ -0,0 +1,36 @@
+export const links = [
+  {
+    category: "Open Source",
+    description: "Fullstack React framework",
+    id: 1,
+    imageUrl: "https://github.com/vercel.png",
+    title: "Next.js",
+    url: "https://nextjs.org",
+  },
+  {
+    category: "Open Source",
+    description: "Next Generation ORM for TypeScript and JavaScript",
+    id: 2,
+    imageUrl: "https://github.com/prisma.png",
+
+    title: "Prisma",
+    url: "https://prisma.io",
+  },
+  {
+    category: "Open Source",
+    description: "Utility-fist css framework",
+    id: 3,
+    imageUrl: "https://github.com/tailwindlabs.png",
+    title: "TailwindCSS",
+    url: "https://tailwindcss.com",
+  },
+  {
+    category: "Open Source",
+    description: "GraphQL implementation ",
+    id: 4,
+    imageUrl:
+      "https://venturebeat.com/wp-content/uploads/2019/06/4f44eef2-5673-43e5-808c-0e215a3855c9.png?fit=1800%2C1120&strip=all",
+    title: "Apollo GraphQL",
+    url: "https://apollographql.com",
+  },
+];
